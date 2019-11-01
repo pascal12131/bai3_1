e63171fcd95c0e46369b0aba306f9e9a713838bf
Thanhcoder <pascal12131@gmail.com>
2019-11-01 16:46:39 +0700

    phan 5 sua  xoa 2 file activity

--- app/src/main/java/com/example/bai3_1/login.java
+++
@@ -1,6 +0,0 @@
-package com.example.bai3_1;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class login extends AppCompatActivity {
-}
--- app/src/main/java/com/example/bai3_1/send.java
+++
@@ -1,7 +0,0 @@
-package com.example.bai3_1;
-
-import android.app.Activity;
-
-public class send extends Activity {
-    private  final  String Tag = getClass().getSimpleName();
-}
